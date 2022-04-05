deeb24a9ce609d7c569f8f281eeea50cc486bbf3
Yuri Andrei Gelsleichter <42816926+Gelsleichter@users.noreply.github.com>
2022-04-05 17:35:37 +0200

    Add title

--- noisy_df.R
+++ noisy_df.R
@@ -1,20 +1,32 @@
+### ----------------------------------------------------------------------------
+### Yuri Gelsleichter, April 2022 - Hungary
+#   _   _       _                 _       _        
+#  | \ | | ___ (_)___  ___     __| | __ _| |_ __ _ 
+#  |  \| |/ _ \| / __|/ _ \   / _` |/ _` | __/ _` |
+#  | |\  | (_) | \__ \  __/  | (_| | (_| | || (_| |
+#  |_| \_|\___/|_|___/\___|___\__,_|\__,_|\__\__,_|
+#                        |_____|      
+# +--- slant ::: https:\\www.askapache.com/online-tools/figlet-ascii/
+### ----------------------------------------------------------------------------
+
 # Make the results non-random
 set.seed(2020)    
 
-##########################################################################
+################################################################################
 # Generate example data
 data <- data.frame(col_test= c(100,100,100,100,100), 
                    col_train= c(80,80,80,80,80))
 
-##########################################################################
+################################################################################
 # Modify ONLY one colum of the data with R BASE
 data$col_test <- jitter(data$col_test, amount= 90) 
 data
 # Source: https://www.geeksforgeeks.org/how-to-use-the-jitter-function-in-r-for-scatterplots/
 
+###----------------------------------------------------------------###
 ###+++ play with the amount to change the range of modification +++###
 
-##########################################################################
+################################################################################
 # modify All columns of the data
 
 # Method 1 with apply (from R base)
@@ -26,38 +38,8 @@ library(dplyr) # Load library
 data_mod <- mutate_all(data, function(x) jitter(x, amount= 90))
 data_mod
 
-##########################################################################
-# round the data by 2 decimals
-data_mod <- apply(data_mod, 2, function(x) round(x, 2))
-data_mod# Make the results non-random
-set.seed(2020)    
-
-##########################################################################
-# Generate example data
-data <- data.frame(col_test= c(100,100,100,100,100), 
-                   col_train= c(80,80,80,80,80))
-
-##########################################################################
-# Modify ONLY one colum of the data with R BASE
-data$col_test <- jitter(data$col_test, amount= 90) 
-data
-# Source: https://www.geeksforgeeks.org/how-to-use-the-jitter-function-in-r-for-scatterplots/
-
-###+++ play with the amount to change the range of modification +++###
-
-##########################################################################
-# modify All columns of the data
-
-# Method 1 with apply (from R base)
-data_mod <- apply(data, 2, function(x) jitter(x, amount= 90))
-data_mod
-
-# Or method 2 with mutate (from package dplyr)
-library(dplyr) # Load library
-data_mod <- mutate_all(data, function(x) jitter(x, amount= 90))
-data_mod
-
-##########################################################################
+################################################################################
 # round the data by 2 decimals
 data_mod <- apply(data_mod, 2, function(x) round(x, 2))
 data_mod
+
